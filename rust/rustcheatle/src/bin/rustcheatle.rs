da584fd1c92f0853b6f7a6a3276536b398d9e8eb
Chuck Ocheret <ocheret@panix.com>
2023-01-25 01:41:54 -0500

    First working Rust version.

--- rust/rustcheatle/src/bin/rustcheatle.rs
+++ rust/rustcheatle/src/bin/rustcheatle.rs
@@ -1,5 +1,86 @@
-use rustcheatle::cheatle;
+use std::io::Write;
+
+use rustcheatle::cheatle::{self, Cheatle};
+use std::io;
+
+fn prompt() {
+    print!("cheatle> ");
+    io::stdout().flush().ok();
+}
+
+fn print_help() {
+    println!("reset : start a new cheating session");
+    println!("- <LETTERS> : letters not in solution");
+    println!("-N <LETTER> : letter in solution but not in Nth position");
+    println!("+N <LETTER> : letter is in Nth position");
+    println!("list : list remaining solutions");
+    println!("help : this messsage");
+}
 
 fn main() {
     cheatle::do_something();
+    let mut chtl = Cheatle::new();
+    prompt();
+    let mut buf = String::new();
+    while let Ok(count) = io::stdin().read_line(&mut buf) {
+        if count == 0 {
+            // A count of 0 means we have reached EOF
+            break;
+        }
+        let line = buf.trim().to_lowercase();
+        let tokens: Vec<&str> = line.split_whitespace().collect();
+        if tokens.len() == 0 || tokens[0] == "" {
+            prompt();
+            continue;
+        }
+        let command = tokens[0];
+        if count == 0 {
+            break;
+        }
+        match command {
+            "reset" => chtl.reset(),
+            "help" => print_help(),
+            "list" => {
+                chtl.filter();
+                let words = chtl.get_words();
+                println!("{}", words.join("\n"));
+                println!("count: {}", words.len());
+            }
+            "-" => {
+                if tokens.len() < 2 {
+                    print_help();
+                    continue;
+                }
+                for c in tokens[1].chars() {
+                    chtl.not_in_word(c);
+                }
+            }
+            "-1" | "-2" | "-3" | "-4" | "-5" => {
+                // XXX - if a letter is misplaced more than 1 time in a single
+                // guess then the letter must be repeated at least that many
+                // times in the word. What's a good way to capture this here?
+                if tokens.len() != 2 {
+                    print_help();
+                    continue;
+                }
+                let pos = command[1..].parse::<usize>().unwrap() - 1;
+                chtl.misplaced_in_word(pos, tokens[1].chars().collect::<Vec<char>>()[0]);
+            }
+            "+1" | "+2" | "+3" | "+4" | "+5" => {
+                if tokens.len() != 2 {
+                    print_help();
+                    continue;
+                }
+                let pos = command[1..].parse::<usize>().unwrap() - 1;
+                chtl.placed_in_word(pos, tokens[1].chars().collect::<Vec<char>>()[0]);
+            }
+            _ => {
+                println!("Invalid command");
+                print_help();
+            }
+        };
+        buf = String::new();
+        prompt();
+    }
+    println!("\nBye");
 }
--- rust/rustcheatle/src/cheatle.rs
+++ rust/rustcheatle/src/cheatle.rs
@@ -1,3 +1,108 @@
+use regex::Regex;
+use std::collections::HashSet;
+use std::sync::Once;
+
 pub fn do_something() {
     println!("Doing something");
+}
+
+// Regular expression for the entire alphabet
+const ALPHABET: &str = "[abcdefghijklmnopqrstuvwxyz]";
+
+// The bytes of the entire dictionary as a single string
+static RAW_DICTIONARY: &str = include_str!("./fivechars.txt");
+
+// Vector of all of the words in the dictionary
+static mut WORDS: Vec<&str> = Vec::new();
+
+static START: Once = Once::new();
+
+#[derive(Debug)]
+pub struct Cheatle {
+    remaining: [String; 5],
+    required: HashSet<char>,
+    indices: Vec<usize>,
+}
+
+impl Cheatle {
+    pub fn new() -> Self {
+        unsafe {
+            START.call_once(|| {
+                WORDS = RAW_DICTIONARY.trim().split("\n").collect();
+            });
+        }
+        let words_len = unsafe { WORDS.len() };
+        Cheatle {
+            remaining: [
+                ALPHABET.to_string(),
+                ALPHABET.to_string(),
+                ALPHABET.to_string(),
+                ALPHABET.to_string(),
+                ALPHABET.to_string(),
+            ],
+            required: HashSet::<char>::new(),
+            indices: (0..words_len).collect(),
+        }
+    }
+
+    pub fn reset(&mut self) {
+        *self = Cheatle::new();
+    }
+
+    pub fn remove_letter(&mut self, pos: usize, letter: char) {
+        self.remaining[pos] = self.remaining[pos].replace(&letter.to_string(), "");
+    }
+
+    pub fn not_in_word(&mut self, letter: char) {
+        for pos in 0..(self.remaining.len()) {
+            self.remove_letter(pos, letter);
+        }
+    }
+
+    pub fn placed_in_word(&mut self, pos: usize, letter: char) {
+        self.remaining[pos] = letter.to_string();
+        if self.required.contains(&letter) {
+            self.required.remove(&letter);
+        }
+    }
+
+    pub fn misplaced_in_word(&mut self, pos: usize, letter: char) {
+        self.remove_letter(pos, letter);
+        self.required.insert(letter);
+    }
+
+    fn has_required(&self, word: &str) -> bool {
+        for c in &self.required {
+            if !word.contains(&(*c).to_string()) {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn filter(&mut self) {
+        let expr = self.remaining.join("");
+        let prog = Regex::new(&expr).unwrap();
+        let mut filtered = Vec::<usize>::new();
+        // XXX - can't seem to do this with ...iter().filter()...
+        for i in &self.indices {
+            unsafe {
+                let w = WORDS[*i];
+                if self.has_required(w) && prog.is_match(w) {
+                    filtered.push(*i);
+                }
+            }
+        }
+        self.indices = filtered;
+    }
+
+    pub fn get_words(&self) -> Vec<&str> {
+        let mut result = Vec::<&str>::new();
+        for i in &self.indices {
+            unsafe {
+                result.push(WORDS[*i]);
+            }
+        }
+        result
+    }
 }
